b54bba4857bde2ac0bf05f21832e6466931d85e5
mrprg <mohamadrezaasadi322@gmail.com>
2024-12-16 15:09:34 +0330

    add some styles for swiper

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,79 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileMenu.classList.add("hidden");
   });
 });
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1.35,
+  spaceBetween: 0,
+  dynamicBullets: true,
+  pagination: {
+    el: ".swiper-pagination", // Pagination
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", // Next button
+    prevEl: ".swiper-button-prev", // Previous button
+  },
+  breakpoints: {
+    // Responsive breakpoints
+    680: {
+      slidesPerView: 1.85, // Show 1 slide at a time on small screens
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2.5, // Show 2 slides at a time on medium screens
+      spaceBetween: 130,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 100,
+    },
+    1660: {
+      slidesPerView: 6,
+      spaceBetween: 80,
+    },
+  },
+});
+
+var swiper = new Swiper(".swiper-books-container", {
+  slidesPerView: 1.1,
+  spaceBetween: 0,
+  dynamicBullets: true,
+  pagination: {
+    el: ".swiper-pagination", // Pagination
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", // Next button
+    prevEl: ".swiper-button-prev", // Previous button
+  },
+  breakpoints: {
+    // Responsive breakpoints
+    680: {
+      slidesPerView: 1.35, // Show 1 slide at a time on small screens
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 1.75, // Show 2 slides at a time on medium screens
+      spaceBetween: 130,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 3.5,
+      spaceBetween: 100,
+    },
+    1660: {
+      slidesPerView: 4.25,
+      spaceBetween: 0,
+    },
+  },
+});
