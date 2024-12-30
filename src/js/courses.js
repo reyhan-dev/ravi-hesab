ca349e23bf20f0f46169663abb2343d81a9a6fc7
mrprg <mohamadrezaasadi322@gmail.com>
2024-12-30 17:14:06 +0330

    add new pages and logics

--- src/js/courses.js
+++ src/js/courses.js
@@ -18,173 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1.5,
-  spaceBetween: 24,
-  dynamicBullets: true,
-  pagination: {
-    el: ".swiper-pagination", // Pagination
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next", // Next button
-    prevEl: ".swiper-button-prev", // Previous button
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2.5, // Show 2 slides at a time on medium screens
-      spaceBetween: 24,
-    },
-    1024: {
-      slidesPerView: 2.5,
-      spaceBetween: 24,
-    },
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-
-    1440: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    },
-
-    1920: {
-      slidesPerView: 5,
-      spaceBetween: 24,
-    },
-  },
-});
-
-var swiper = new Swiper(".swiper-blogs-container", {
-  slidesPerView: 1.42,
-  spaceBetween: 24,
-  dynamicBullets: true,
-  pagination: {
-    el: ".swiper-pagination", // Pagination
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next", // Next button
-    prevEl: ".swiper-button-prev", // Previous button
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2.5, // Show 2 slides at a time on medium screens
-      spaceBetween: 130,
-    },
-    1024: {
-      slidesPerView: 2.3,
-      spaceBetween: 24,
-    },
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-
-    1440: {
-      slidesPerView: 3.5,
-      spaceBetween: 24,
-    },
-    1660: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    },
-
-    
-  },
-});
-
-var swiper = new Swiper(".swiper-books-container", {
-  slidesPerView: 1.065,
-  spaceBetween: 24,
-  dynamicBullets: true,
-  pagination: {
-    el: ".swiper-pagination", // Pagination
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next", // Next button
-    prevEl: ".swiper-button-prev", // Previous button
-  },
-  breakpoints: {
-    // Responsive breakpoints
-    400: {
-      slidesPerView: 1.1, // Show 1 slide at a time on small screens
-      spaceBetween: 24,
-    },
-    768: {
-      slidesPerView: 1.75, // Show 2 slides at a time on medium screens
-      spaceBetween: 130,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 25,
-    },
-    1280: {
-      slidesPerView: 2.5,
-      spaceBetween: 25,
-    },
-
-    1350: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-
-    1440: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-    1660: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-
-    1920: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-  },
-});
-
-var swiper = new Swiper(".swiper-subscription-container", {
-  slidesPerView: 1.25,
-  spaceBetween: 24,
-  dynamicBullets: true,
-  pagination: {
-    el: ".swiper-pagination", // Pagination
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next", // Next button
-    prevEl: ".swiper-button-prev", // Previous button
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1.75, // Show 2 slides at a time on medium screens
-      spaceBetween: 130,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 24,
-    },
-    1280: {
-      slidesPerView: 2.5,
-      spaceBetween: 24,
-    },
-
-    1440: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-  },
-});
-
-// Get references to dropdown search *desktop* elements
 const dropdownButton = document.getElementById(
   "dropdownButton"
 );
@@ -386,9 +219,43 @@ mobileDropdownSearch.addEventListener(
   }
 );
 
-// dropdownSearch.addEventListener("mouseleave", function () {
-//   dropdownSearch.classList.add("hidden"); // Hide when not hovering
-// });
+const filterDropdownToggle = document.getElementById(
+  "filterDropdownToggle"
+);
+const filterDropdownMenu = document.getElementById(
+  "filterDropdownMenu"
+);
+const filterDropdownLabel = document.getElementById(
+  "filterDropdownLabel"
+);
+
+filterDropdownToggle.addEventListener("click", () => {
+  filterDropdownMenu.classList.toggle("hidden");
+  filterDropdownMenu.classList.toggle("flex");
+});
+
+// Add event listeners to buttons
+document
+  .querySelectorAll("#dropdownMenu button")
+  .forEach((button) => {
+    button.addEventListener("click", () => {
+      const selectedValue =
+        button.getAttribute("data-value");
+      filterDropdownLabel.textContent = selectedValue; // Update the label
+      filterDropdownLabel.classList.add("hidden"); // Hide the dropdown after selection
+    });
+  });
+
+// Close the dropdown when clicking outside of it
+document.addEventListener("click", (event) => {
+  const isClickInsideDropdown =
+    dropdownToggle.contains(event.target) ||
+    dropdownMenu.contains(event.target);
+  if (!isClickInsideDropdown) {
+    dropdownMenu.classList.add("hidden");
+    dropdownMenu.classList.remove("flex");
+  }
+});
 
 function toggleAccordion(id) {
   const content = document.getElementById(id);
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,26 @@ module.exports = {
         borderColor: "#EFEFEF",
         secondary: "#0C0D0F",
         primary: "#FFBB00",
+        primary_lighten: "#FFF8E0",
         muted: "#949494",
         danger: "#FF3431",
         secondary_lighten: "#E8E8E6",
+        success: "#14AE5C",
+        success_lighten: "#E2F5EB",
       },
       borderRadius: {
         md: "10px",
       },
       fontFamily: {
         yekan: ["YekanBakh", "sans-serif"],
+        danstevis: ["danstevis"],
+        fanoos: ["fanoos"],
       },
       boxShadow: {
         custom: "0px 4px 16px 0px rgba(21, 16, 0, 0.2)",
       },
       padding: {
-        extralg: "70px",
+        extralg: "120px",
       },
     },
   },
